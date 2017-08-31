6447287c44f308f15773b0d915032dae312c6360
Hamza Khawaja <hkhawaja3@gmail.com>
2017-08-31 16:01:11 -0500

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,20 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'hIJ1EzQ5q9x2djDzqq94l8oeE',
+  consumer_secret: 'ShCZTmUKNNzddBhN4a9bWzig4oN3q3D0WU2AdqLfSiRz44gvyP',
+  access_token_key: '903336791373225985-H8Pcx2y4hBD34bkYdmIh4yJyRbihHi7',
+  access_token_secret: '0pQBXjyuushDqrlgj65xnuRnGzoSyeYbrsxXln8QWQW36',
+}
+
+
+// spotify
+
+// Client ID
+// 9326288284ef417e96b0222935c7f6b8
+
+
+// Client Secret
+// 288134697297430bad1b7a38c940a1d2
+
+// https://www.npmjs.com/package/node-spotify-api
